82438897202aa582e4e73c624813e0ea130ab347
ShaylynKM <shaylynmelin@gmail.com>
2023-10-08 21:51:08 -0600

    Jump key + Added levels
    Changed jump key from spacebar to up arrow and added a folder with the 4 scenes for our levels

--- NewKidOnTheBlock/Assets/Scripts/Player_Controller.cs
+++ NewKidOnTheBlock/Assets/Scripts/Player_Controller.cs
@@ -65,7 +65,7 @@ public class Player_Controller : MonoBehaviour
      void Update()
     {
         
-       if(Input.GetKeyDown(KeyCode.Space))
+       if(Input.GetKeyDown(KeyCode.UpArrow))
        {
         _jumped = true;
        }
