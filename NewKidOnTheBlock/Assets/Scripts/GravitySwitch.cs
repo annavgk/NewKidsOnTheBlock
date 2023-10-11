f2e1668301b8e3db1bf4586e2b6442b9a201c7e6
Blake Laprade <lapradeblake@gmail.com>
2023-10-11 06:56:38 -0600

    All player features implemented
    player can use triangle and circle abilites. needs polish. values need to be tweaked to suit levels

---
+++ NewKidOnTheBlock/Assets/Scripts/GravitySwitch.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySwitch : MonoBehaviour
+{
+
+    //[SerializeField]private  BoxCollider2D _collider;
+    [SerializeField] private string direction;
+
+    [SerializeField] private string secondaryDirection;
+    // Start is called before the first frame update
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<TriangleController>().active == true)
+        {
+            switch (direction)
+            {
+                case "up":
+                    if (Physics2D.gravity == new Vector2(0, 9.8f))
+                    {
+                        Secondary();
+                        break;
+                    }
+                    Physics2D.gravity = new Vector2(0, 9.8f);
+                    break;
+                case "down":
+                    if (Physics2D.gravity == new Vector2(0, -9.8f))
+                    {
+                        Secondary();
+                        break;
+                    }
+                    Physics2D.gravity = new Vector2(0, -9.8f);
+                    break;
+                case "left":
+                    if (Physics2D.gravity == new Vector2(-9.8f, 0))
+                    {
+                        Secondary();
+                        break;
+                    }
+                    Physics2D.gravity = new Vector2(-9.8f, 0);
+                    break;
+                case "right":
+                    if (Physics2D.gravity == new Vector2(9.8f, 0))
+                    {
+                        Secondary();
+                        break;
+                    }
+                    Physics2D.gravity = new Vector2(9.8f, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+     }
+
+    private void Secondary()
+    {
+        switch (secondaryDirection)
+        {
+            case "up":
+                
+                Physics2D.gravity = new Vector2(0, 9.8f);
+                break;
+            case "down":
+                
+                Physics2D.gravity = new Vector2(0, -9.8f);
+                break;
+            case "left":
+                
+                Physics2D.gravity = new Vector2(-9.8f, 0);
+                break;
+            case "right":
+                
+                Physics2D.gravity = new Vector2(9.8f, 0);
+                break;
+            default:
+                Physics2D.gravity = Physics2D.gravity;
+                break;
+                
+        }
+    }
+}
+   
+
