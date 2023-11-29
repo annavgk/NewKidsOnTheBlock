0dfefdd22e05462c1c320431d6ff09e358864442
Blake Laprade <lapradeblake@gmail.com>
2023-11-28 18:26:10 -0700

    Level9 blockout
    Prefabs for setting up triangles edge turns

--- NewKidOnTheBlock/Assets/Scripts/TriangleController.cs
+++ NewKidOnTheBlock/Assets/Scripts/TriangleController.cs
@@ -22,12 +22,16 @@ public class TriangleController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.X)) //swaps player object
         {
+            _square.transform.position = transform.position;
+            _circle.transform.position = transform.position;
             _circle.gameObject.SetActive(true);
             Physics2D.gravity = new Vector2(0, -9.8f);
             gameObject.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.Z))
         {
+            _square.transform.position = transform.position;
+            _circle.transform.position = transform.position;
             _square.gameObject.SetActive(true);
             Physics2D.gravity = new Vector2(0, -9.8f);
             gameObject.SetActive(false);
